6c84608b0429a49ff7b5292b6dbadb1fdefee572
Rohit Kushwaha <113577083+Rohitkushwaha7389@users.noreply.github.com>
2023-06-28 12:46:08 +0530

    Create  Assignment3_6.java

---
+++ Assignment3_6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Assignment3_6 {
+    public static void main(String[] args) {
+        int k[] = { 1, 2,3, 4, 5, 5, 4, 1, 3 };
+        Arrays.sort(k);
+        System.out.println(findSingle(k,k.length-1));
+ 
+    }
+    private static int findSingle(int k[],int n){
+        
+        for (int i = 0; i < k.length-1; i++) {
+
+            if (k[i] == k[i + 1]) {
+                k[i] = -1;
+                k[i+1]=-1;
+               
+            }
+        }
+         Arrays.sort(k);
+          return k[k.length-1];
+    }
+
+}

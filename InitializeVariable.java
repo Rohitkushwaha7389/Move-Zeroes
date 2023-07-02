f14c29b491f5709db777ede362106c7e1e466d3e
Rohit Kushwaha <113577083+Rohitkushwaha7389@users.noreply.github.com>
2023-07-02 14:28:25 +0530

    Create  InitializeVariable.java

---
+++ InitializeVariable.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class InitializeVariable{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number/int ");
+         int n=sc.nextInt();
+
+        System.out.println("Enter number/double  ");
+        double d=sc.nextDouble();
+
+        System.out.println("Enter String ");
+        String str=sc.next();
+
+        System.out.println(n);
+        System.out.println(d);
+        System.out.println(str);
+
+    }
+}
